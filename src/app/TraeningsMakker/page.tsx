b630aaaaaee9946914c9b8ee005216b18d80fbe4
MHEM11 <92570521+MHEM11@users.noreply.github.com>
2023-11-04 01:29:05 +0100

    small edits on TM FP

--- src/app/TraeningsMakker/page.tsx
+++ src/app/TraeningsMakker/page.tsx
@@ -5,33 +5,33 @@ import { MdOutlineKeyboardArrowLeft as BackArrow } from "react-icons/md"
 const TraeningsMakker = () => {
   return (
     <main className="bg-gradient-to-b from-slate-700 to-slate-500 text-white min-h-screen">
-      <header className="flex justify-center">      
-        <Link href="./Projects" className='className="absolute top-1 left-1 border rounded-md h-10 w-10 items-center"'>
+      <header className="flex justify-center items-center">      
+        <Link href="./Projects" className="absolute top-2 left-2 border rounded-md">
           <BackArrow size="2.5rem"></BackArrow>
         </Link>
-        <h2 className="text-2xl pt-2">TræningsMakker</h2>
+        <h2 className="text-3xl pt-3 items-center">TræningsMakker</h2>
       </header>
       <div className="flex flex-col justify-center items-center">
-        <div className="flex flex-col w-full items-center pt-5 space-y-7">
-          <button className="rounded-lg bg-slate-800 w-11/12 h-12 border-2">
+        <div className="flex flex-col w-full items-center pt-12 space-y-7">
+          <button className="rounded-lg bg-slate-800 w-11/12 h-14 border-2 text-xl">
             Pull
           </button>
-          <button className="rounded-lg bg-slate-800 w-11/12 h-12 border-2">
+          <button className="rounded-lg bg-slate-800 w-11/12 h-14 border-2 text-xl">
             Push
           </button>
-          <button className="rounded-lg bg-slate-800 w-11/12 h-12 border-2">
+          <button className="rounded-lg bg-slate-800 w-11/12 h-14 border-2 text-xl">
             Leg
           </button>
-          <button className="rounded-lg bg-slate-800 w-11/12 h-12 border-2">
+          <button className="rounded-lg bg-slate-800 w-11/12 h-14 border-2 text-xl">
             New
           </button>
-          <button className="rounded-lg bg-slate-800 w-11/12 h-12 border-2">
+          <button className="rounded-lg bg-slate-800 w-11/12 h-14 border-2 text-xl">
             Profile
           </button>
-          <button className="rounded-lg bg-slate-800 w-11/12 h-12 border-2">
+          <button className="rounded-lg bg-slate-800 w-11/12 h-14 border-2 text-xl">
             Calender 
           </button>
-        </div>
+        </div>  
       </div>
     </main>
   )
